1ad1a6c3cc2fa6cf76ce979ff8bdc030334a4ec7
Jim Jack <jim_jack@Jims-MacBook-Pro.local>
2018-11-05 16:58:23 -0500

    adding actors/roles lab

--- models.py
+++ models.py
@@ -7,11 +7,18 @@ Base = declarative_base()
 
 # write the Role and Actor classes below
 
+class Role(Base): #child
+    __tablename__ = 'roles'
+    id=Column(Integer,primary_key=True)
+    character=Column(Text)
+    actor_id = Column(Integer,ForeignKey('actors.id'))
+    actors = relationship("Actor",back_populates='roles')
 
-
-
-
-
+class Actor(Base): #parent
+    __tablename__ = 'actors'
+    id=Column(Integer,primary_key=True)
+    name=Column(Text)
+    roles = relationship("Role",order_by=Role.id,back_populates='actors')
 
 engine = create_engine('sqlite:///actors.db')
 Base.metadata.create_all(engine)
--- seed.py
+++ seed.py
@@ -7,3 +7,13 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # add and commit the actors and roles below
+tom_hanks = Actor(name="Tom Hanks")
+gwyneth_paltrow = Actor(name="Gwyneth Paltrow")
+daniel_craig = Actor(name="Daniel Craig")
+
+tom_hanks.roles = [Role(character ="Forrest Gump"),Role(character ="Jim Lovell"),Role(character ="Woody"),Role(character ="Robert Langdon")]
+gwyneth_paltrow.roles = [Role(character ="Pepper Potts"),Role(character ="Margot Tenenbaum")]
+daniel_craig.roles = [Role(character ="James Bond"),Role(character ="Young James Bond")]
+
+session.add_all([tom_hanks,gwyneth_paltrow,daniel_craig])
+session.commit()
